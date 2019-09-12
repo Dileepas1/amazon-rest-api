256e160fbf1508b4830e372d4169b83b1c067c1f
dileep1998 <dileepa.as@ymedialabs.com>
2019-09-12 15:54:54 +0530

    SonarLint test is done

--- src/main/java/com/ecommerce/amazon_ecommerce/configuration/WebSecurityConfig.java
+++ src/main/java/com/ecommerce/amazon_ecommerce/configuration/WebSecurityConfig.java
@@ -57,14 +57,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/**").authenticated()
                 .and()
                 .formLogin().and().
-//        anyRequest().authenticated().and().
 
-// make sure we use stateless session; session won't be used to
-// store user's state.
         exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
-// Add a filter to validate the tokens with every request
         httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
     }
 }
